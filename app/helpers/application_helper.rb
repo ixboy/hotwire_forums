dd66a92d1ae7d52e1fed7f1e2e266fdad0ad2b63
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2023-12-21 02:27:13 -0500

    create helpers for navbar links and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def display_msg
+    concat content_tag(:div, alert, class: 'alert alert-danger') if alert.present?
+    content_tag(:div, notice, class: 'alert alert-success') if notice.present?
+  end
+
+  def links_when_logged_in
+    return unless signed_in?
+
+    concat  link_to Current.user.username, edit_user_registration_path, class: 'nav-item nav-link'
+    link_to 'log out', destroy_user_session_path, method: :delete, class: 'nav-item nav-link',
+                                                  data: { turbo_method: :delete }
+  end
+
+  def links_when_logged_out
+    return if signed_in?
+
+    concat link_to 'Sign up', new_user_registration_path, class: 'nav-item nav-link'
+    link_to 'Log in', new_user_session_path, class: 'nav-item nav-link'
+  end
 end
