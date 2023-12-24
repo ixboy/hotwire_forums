b3c7168a8e66ae61db27c2ecb5184d2e43af608f
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2023-12-24 02:05:24 -0500

    add discussion new and create actions to controller and setup the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: [:index]
+  resources :discussions, only: %i[index new create destroy]
   root 'main#index'
 end
