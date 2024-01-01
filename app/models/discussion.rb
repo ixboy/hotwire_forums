b85e3b3599fd3d4752fc0523138d39f33e6d1aad
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2024-01-01 07:09:17 -0500

    add real time discussion functionality

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,6 +2,9 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
   validates :name, presence: true
+  after_create_commit -> { broadcast_prepend_to 'discussion' }
+  after_update_commit -> { broadcast_replace_to 'discussion' }
+  after_destroy_commit -> { broadcast_remove_to 'discussion' }
 
   def to_param
     "#{id}-#{name.downcase.to_s[0..100]}".parameterize
