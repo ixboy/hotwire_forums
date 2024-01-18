c6ccd5b8456965d13eb369f3100955d32c91db53
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2024-01-18 00:58:05 -0500

    add categories to discussion index

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,8 @@
 module CategoriesHelper
+  def discussion_count(category)
+    return unless category.discussions_count
+
+    content_tag(:span, category.discussions_count,
+                class: 'badge bg-secondary rounded-pill text-light')
+  end
 end
