8562a31b47b5a511a873abe5d4643a660e8ee988
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2024-01-22 01:15:18 -0500

    install pagy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  include Pagy::Frontend
   def display_msg
     concat content_tag(:div, alert, class: 'alert alert-danger') if alert.present?
     content_tag(:div, notice, class: 'alert alert-success') if notice.present?
