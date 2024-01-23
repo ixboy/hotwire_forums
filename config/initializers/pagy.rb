b07b7d1941784cc63a3c7abfad5bb3674bf93498
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2024-01-23 12:37:02 -0500

    paginate posts on discussion show

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -2,7 +2,7 @@ require 'pagy/extras/bootstrap'
 require 'pagy/extras/overflow'
 
 # Optionally override some pagy default with your own in the pagy initializer
-Pagy::DEFAULT[:items] = 7 # items per page
+Pagy::DEFAULT[:items] = 9 # items per page
 Pagy::DEFAULT[:size]  = [1, 4, 4, 1] # nav bar links
 # Better user experience handled automatically
 Pagy::DEFAULT[:overflow] = :last_page
